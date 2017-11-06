daa7760f07847997fed62f4a018116ea847a4c62
levin <levinnat88@gmail.com>
2017-11-06 11:36:57 -0500

    Added ArrayMethods starter code.

---
+++ ClassWork/src/lab_3_3_arrays/ArrayMethods.java
@@ -0,0 +1,31 @@
+package lab_3_3_arrays;
+
+public class ArrayMethods {
+	
+	public static int[] removeDuplicates(int[] list)
+	{
+		int[] answer = new int[0];
+		//return a new array with the duplicates removed.
+		return answer;
+	}
+
+	public static int[][] productArray(int[] arr1, int[] arr2)
+	{
+		int[][] answer = new int[0][0];
+		return answer;
+	
+	}
+	
+	public static int[][] pascalTriange(int n)
+	{
+		int[][] answer = new int[0][0];
+		return answer;
+	}
+	
+	public static void printPascalTriangle(int[][] pTriangle)
+	{
+		
+	}
+}
+
+
